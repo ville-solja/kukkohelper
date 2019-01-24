603779aa1cd441c3e00c7bae1e8d089b423f1694
Ville Solja <ville.solja@gmail.com>
2019-01-24 21:00:22 +0200

    Removed redundant startup.sh as script is being containerized

--- startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ $(ps aux | grep 'kukkohelper.py' | grep -v grep  | wc -l) -gt 0 ] 
-then
-	echo "Already running"
-	exit
-fi
-echo "Launching kukkohelper"
-python3 /opt/scripts/kukkohelper/kukkohelper.py
