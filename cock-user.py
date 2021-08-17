5f34cb65f8435d4ff51ba434e6137e1fc224eb9a
makkaramato <88145434+makkaramato@users.noreply.github.com>
2021-08-17 14:44:36 +0300

    Add files via upload

--- cock-user.py
+++ cock-user.py
@@ -13,7 +13,7 @@ class cock_helper(commands.Cog):
     @commands.Cog.listener()
     async def on_guild_channel_create(self, channel):
         logger.info("Detected new channel created: " + channel.name)
-        if(bool(re.match("game-", channel.name))):
+        if(bool(re.match("club-", channel.name))):
             role = discord.utils.get(channel.guild.roles, name = channel.name)
             logger.info("Found role " + role.name)
             if(role==None):
@@ -34,27 +34,27 @@ class cock_helper(commands.Cog):
            ## await ctx.send("Bot functions currently offline.")
 
 
-    @commands.command(name="join", brief = "Join a group.", help = "Join a gaming group. Group/Role name is case sensitive!!")
+    @commands.command(name="join", brief = "Join a club.", help = "Join a club/group. Name is case sensitive probably.")
     async def join(self, ctx, group):
         logger.info("command join called")
         member = ctx.message.author
 
-        is_game = bool(re.match("game",group,  re.IGNORECASE))
+        is_club = bool(re.match("club",group,  re.IGNORECASE))
 
-        if(is_game):
+        if(is_club):
             group = group.lower()
             role = discord.utils.get(ctx.guild.roles, name = group)
             if (role == None):
-                await ctx.send("Group not found. Please note that group name is case sensitive.")
+                await ctx.send("Club not found. Please note that name is case sensitive.")
                 logger.info("Group " + group + " not found on server.")
             else:
                 await member.add_roles(role)
                 logger.info("Added " + str(ctx.message.author) + " to group " + str(role))
                 await ctx.send("I added you to group " + str(role))
         else:
-            await ctx.send("You can only add groups with Game- prefix currently.")
+            await ctx.send("You can only add groups with club- prefix currently.")
 
-    @commands.command(name="quit", brief= "Leave a group.", help = "Leave a gaming group. Group/Role name is case sensitive!!")
+    @commands.command(name="quit", brief= "Leave a club.", help = "Leave a group/club.")
     async def quit(self, ctx, group):
         logger.info("command quit called")
         member = ctx.message.author
@@ -69,7 +69,7 @@ class cock_helper(commands.Cog):
             logger.info("Role " + str(role) + " not found for user " + str(member))
             await ctx.send("Could not find you in " + group)
 
-    @commands.command(name="list", brief = "List gaming groups.")
+    @commands.command(name="list", brief = "List clubs.")
     async def list(self, ctx):
         logger.info("command list called")
         roles = ctx.guild.roles
@@ -77,18 +77,18 @@ class cock_helper(commands.Cog):
         message = "```\n"
 
         for role in roles:
-            if(bool(re.match("game", str(role),  re.IGNORECASE))):
+            if(bool(re.match("club", str(role),  re.IGNORECASE))):
                 message = message + str(role) + "\n"
         message = message + "```"
         logger.info(message)
         await ctx.send(message)
 
-    @commands.command(name="create_club", brief = "Create new gaming group", help = "Create new gaming group. Must have game- prefix.", aliases=['create', 'cc'])
+    @commands.command(name="create_club", brief = "Create new club", help = "Create new club/group. Must have club- prefix.", aliases=['create', 'cc'])
     async def create_club(self, ctx, group):
         logger.info("command create club called")
         
         group = group.lower()
-        if(bool(re.match("game-", group))):
+        if(bool(re.match("club-", group))):
             role = discord.utils.get(ctx.guild.roles, name = group)
             if (role == None):
                 guild = ctx.guild
@@ -96,18 +96,18 @@ class cock_helper(commands.Cog):
                 overwrites = {guild.default_role: discord.PermissionOverwrite(read_messages=False)}
                 category = discord.utils.get(ctx.guild.categories, name = "CLUBS")
                 await guild.create_text_channel(group, overwrites = overwrites, category = category)
-                await ctx.send("Gayming group " + group + " created for you! Use !list and !join to join now.")
+                await ctx.send("Club " + group + " created for you! Use !list and !join to join now.")
             else:
-                await ctx.send("Group exist already dumkopf.")
+                await ctx.send("Club exist already dumkopf.")
         else:
-            await ctx.send("Group name must have game- prefix. Try again nakkisormi.")  
+            await ctx.send("Club name must have club- prefix. Try again nakkisormi.")  
 
-    @commands.command(name="delete_club", aliases=["del_club", "dc"], brief = "Delete existing gaming group.", help = "Use carefully.")
+    @commands.command(name="delete_club", aliases=["del_club", "dc"], brief = "Delete existing club.", help = "Use carefully.")
     async def delete_club(self, ctx, group):
         logger.info("command delete club called")
         group = group.lower()
 
-        if(bool(re.match("game-", group))):
+        if(bool(re.match("club-", group))):
             channel = discord.utils.get(ctx.guild.channels, name = group)
             role = discord.utils.get(ctx.guild.roles, name = group)
 
@@ -119,7 +119,7 @@ class cock_helper(commands.Cog):
                 await role.delete()
             await ctx.send("All done.")
         else:
-            await ctx.send("Don't try any funny business! Only gaming clubs can be deleted.")
+            await ctx.send("Don't try any funny business! Only channels with club- prefix can be deleted.")
 
 def setup(bot):
     bot.add_cog(cock_helper(bot))
